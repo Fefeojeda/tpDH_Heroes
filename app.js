2871982b4ccfc0c8395b8bafef73bf153ba0f0e6
Fefeojeda <ferj.ojeda@gmail.com>
2021-06-01 19:24:51 -0300

    Agrego HTML, imagenes y barra de navegación.

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = 3030
 const path = require('path')
 const views = path.join(__dirname,'views')
 
-app.get('/',(req,res) => res.sendFile(path.join(views, 'home.html')));
-app.get('/login',(req,res) => res.sendFile(path.join(views, 'login.html')))
-app.get('/register',(req,res) => res.sendFile(path.join(views, 'register.html')))
-app.get('/cart',(req,res) => res.sendFile(path.join(views, 'cart.html')))
-app.get('/detail',(req,res) => res.sendFile(path.join(views, 'detail.html')))
+app.get('/',(req,res) => res.sendFile(path.join(views, 'index.html')));
+app.get('/azathoth',(req,res) => res.sendFile(path.join(views, 'azathoth.html')))
+app.get('/cthulhu',(req,res) => res.sendFile(path.join(views, 'cthulhu.html')))
+app.get('/dagon',(req,res) => res.sendFile(path.join(views, 'dagon.html')))
+app.get('/nyarlathotep',(req,res) => res.sendFile(path.join(views, 'nyarlathotep.html')))
+app.get('/ygolonac',(req,res) => res.sendFile(path.join(views, 'ygolonac.html')))
+app.get('/nightgaunts',(req,res) => res.sendFile(path.join(views, 'nightgaunts.html')))
 
 app.listen(port, () => console.log('Servidor funcionando en puerto '+ port));
 app.use (express.static('public'))
